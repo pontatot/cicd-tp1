030a918278b410162cd1b70b1edbf647cb7f2c9a
hugo.du-peloux <hugo.du-peloux@etu.umontpellier.fr>
2025-03-11 17:31:44 +0100

    Add database schema and Docker configuration for city management API

---
+++ database.sql
@@ -0,0 +1,9 @@
+create table city (
+    id int primary key,
+    department_code varchar(255) not null,
+    insee_code varchar(255),
+    zip_code varchar(255),
+    name varchar(255) not null,
+    lat float not null,
+    lon float not null
+)
